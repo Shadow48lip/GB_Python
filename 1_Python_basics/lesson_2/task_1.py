df6d208c3ad6d511060a141336bf9bc6d63c5983
Shadow48lip <80761520+Shadow48lip@users.noreply.github.com>
2021-07-11 22:56:28 +0300

    Сдача задания 2, Основы Python (#2)
    * Lessons
    
    Add file
    
    * Add file task 4
    
    * Task 4
    
    Закончено последнее задание для урока
    
    * Edit Task 2
    
    Дорешал задачу в более сложным условием
    
    * Edit task 2
    
    * Homework by GB
    
    Как делал преподаватель, сохраню для примеров

---
+++ 1_Python_basics/lesson_2/task_1.py
@@ -0,0 +1,17 @@
+__author__ = 'Дмитрий Назаркин'
+# Выяснить тип результата выражений:
+# 15 * 3
+expression = 15 * 3
+print(expression, type(expression))
+
+# 15 / 3
+expression = 15 / 3
+print(expression, type(expression))
+
+# 15 // 2
+expression = 15 // 2
+print(expression, type(expression))
+
+# 15 ** 2
+expression = 15 ** 2
+print(expression, type(expression))
